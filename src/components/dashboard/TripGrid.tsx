52252f9e6ee6d9afab747942a92da12b1e82363a
LalaIAm <lthorud12@gmail.com>
2024-12-20 17:48:19 +0000

    up

--- src/components/dashboard/TripGrid.tsx
+++ src/components/dashboard/TripGrid.tsx
@@ -98,7 +98,7 @@ const TripGrid = ({
           </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {sortedTrips.map((trip) => (
             <TripCard
               key={trip.id}
